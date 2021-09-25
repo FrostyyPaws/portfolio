abc597420b861e95411533c79c27ab22687c39e1
FrostyyPaws <thefrostyypaws@gmail.com>
2021-09-25 17:05:08 -0400

    Fonts imported. Basic header text styling

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,37 +1,44 @@
 const cards = document.getElementsByClassName('card');
-const highlightTransSpeedMs = 900
+const highlightTransitionSpeedMs = 500
+const hightlightScale = 1.03
+
+function unHighlight(classString) {
+    Array.from(document.getElementsByClassName(classString)).map(e => {
+        e.style.transition = `
+            opacity ${highlightTransitionSpeedMs}ms,
+            transform ${highlightTransitionSpeedMs}ms    
+        `;
+        e.style.opacity = 1;
+        e.style.transform = `scale(1)`
+    })
+}
+
+function highlight(element, classString = "") {
+    let classEles
+    if (classString != "") { classEles = document.getElementsByClassName(classString) }
+    else { classEles = document.getElementsByClassName(element.classList) }
+    Array.from(classEles).map(e => {
+        e.style.transition = `
+            opacity ${highlightTransitionSpeedMs}ms ease-in-out,
+            transform ${highlightTransitionSpeedMs}ms ease-in-out   
+        `
+        if (e == element) {
+            e.style.opacity = 1;
+            e.style.transform = `scale(${hightlightScale})`
+        } else {
+            e.style.opacity = 0.4;
+            e.style.transform = `scale(1)`
+        }
+    })
+}
+
 
 for (let i in cards) {
     let c = cards[i];
     c.onmouseover = () => {
         highlight(c)
     }
-    c.onmouseout = () => { 
-        unHighlight(c)
+    c.onmouseout = () => {
+        unHighlight(c.classList)
     }
-}
-
-function highlight(element, classString = "") {
-    let classEles
-    if (classString != "") { classEles = document.getElementsByClassName(classString) } 
-    else { classEles = document.getElementsByClassName(element.classList) }
-    Array.from(classEles).map(e => {
-        if (e == element) {
-            
-            e.style.opacity = 1;
-        } else {
-            e.style.transition = `${highlightTransSpeedMs}ms`;
-            e.style.opacity = 0.4;
-        }
-    })
-}
-
-function unHighlight(element, classString = ""){ 
-    let classEles
-    if (classString != "") { classEles = document.getElementsByClassName(classString) } 
-    else { classEles = document.getElementsByClassName(element.classList) }
-    Array.from(classEles).map(e =>{ 
-        e.style.transition = "900ms";
-        e.style.opacity = 1;
-    })
 }
